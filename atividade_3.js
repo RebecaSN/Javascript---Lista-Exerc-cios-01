b8ebf7e4d7f75510b5c5022c92219a2236e331ce
RebecaSN <bequiinha123@gmail.com>
2022-02-17 13:08:18 -0300

    first commit

--- atividade_3.js
+++ atividade_3.js
@@ -1,15 +1,14 @@
-var n1= parseInt(prompt('Digite um numero inteiro'))
-var n2= parseInt(prompt('Digite outro numero inteiro'))
+var X = parseInt(prompt('Digite um numero inteiro'))
+var Y = parseInt(prompt('Digite outro numero inteiro'))
 
-console.log('Seu primeiro numero é:'+ n1)
-console.log('Seu segundo numero é:' + n2)
+console.log('Seu primeiro numero é:'+ X)
+console.log('Seu segundo numero é:' + Y)
 
-var n3=n1
-var n4=n2
+var A = X
+X=Y
+Y=A
 
-alert('invertemos os valores de:'+ n1, 'com o de:' + n2,
-      'Agora o seu primeiro numero vale:' +n4, 'E o seu segundo:' +n3)
-
+alert('invertemos os valores e agora o seu primeiro numero vale:'+ X + 'E o seu segundo:' + Y)
 
 
 
